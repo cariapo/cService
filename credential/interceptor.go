44ad2aad5d62cafcea9c60139d674252e35e1ac1
Ichwan Almaza <mail@ichwan.me>
2023-04-19 05:35:28 +0700

    all

--- credential/interceptor.go
+++ credential/interceptor.go
@@ -1,17 +1,27 @@
 package credential
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/cuwand/pondasi/errors"
 	"github.com/cuwand/pondasi/logger"
+	"github.com/cuwand/pondasi/response"
 	"io"
 	"net"
 	"net/http"
+	"strings"
 )
 
 func CredentialErrorInterceptor(resp *http.Response, logger logger.Logger, err error) error {
 	if err, ok := err.(net.Error); ok && err.Timeout() {
-		return errors.BadRequest("Request Timeout")
+		return errors.InternalServerError("Internal Service Request Timeout - Credential Service")
+	}
+
+	if err, ok := err.(net.Error); ok && strings.Contains(err.Error(), "connection refused") {
+		return errors.InternalServerError("Internal Service Down - Credential Service")
+	}
+
+	if err != nil {
+		return err
 	}
 
 	if resp.StatusCode != http.StatusOK {
@@ -23,10 +33,13 @@ func CredentialErrorInterceptor(resp *http.Response, logger logger.Logger, err e
 			return err
 		}
 
-		logger.Error(string(readResp))
-		logger.Error(fmt.Sprintf("%v", resp.StatusCode))
+		var errResp response.ModelError
 
-		return errors.BadRequest("request error.")
+		if err := json.Unmarshal(readResp, &errResp); err != nil {
+			return err
+		}
+
+		return errors.Error(errResp.ErrorString())
 	}
 
 	return nil
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.19
 
 replace github.com/cuwand/pondasi => ../../../go/pondasi
 
-require (
-	github.com/cuwand/pondasi v0.0.0-20220225145018-cfee46d824ba
-)
+require github.com/cuwand/pondasi v0.0.0-20220225145018-cfee46d824ba
+
+require github.com/rs/zerolog v1.26.1 // indirect
